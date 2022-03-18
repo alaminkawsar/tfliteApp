46af5d01b61632bedbf3f53d4904372c7e80d958
alaminkawsar <alaminkawsar8643@gmail.com>
2022-03-18 17:58:20 +0600

    -Add the UI logic

--- tfliteApp/ViewController.swift
+++ tfliteApp/ViewController.swift
@@ -14,13 +14,36 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textResult: UILabel!
     
+    private var modelParser: ModelParser? =
+        ModelParser(modelFileInfo: ModelFile.modelInfo)
+    
     @IBAction func btnGo(_ sender: Any) {
-        
+        doInference()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    private func doInference() {
+        
+        guard let text = txtUserData.text, text.count > 0 else {
+            txtResult.text = "0.00"
+            return
+          }
+        
+        guard let value = Float(text) else {
+            return
+          }
+        
+        guard let result = self.modelParser?.runModel(withInput: value) else {
+            return
+          }
+        
+        txtResult.text = String(format: "%.2f", result)
+
+        
     }
 
 
