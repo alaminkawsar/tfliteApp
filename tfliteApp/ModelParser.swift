e50d4867337ff916d073e596258546e16c886cee
alaminkawsar <alaminkawsar8643@gmail.com>
2022-03-18 17:51:17 +0600

    -Add tensorflow lite -Create the user interface -add and initialize the model inference class -perform the inference

---
+++ tfliteApp/ModelParser.swift
@@ -0,0 +1,95 @@
+//
+//  ModelParser.swift
+//  tfliteApp
+//
+//  Created by Khayrul on 3/18/22.
+//
+
+import Foundation
+import TensorFlowLite
+
+// pass the reference to add model.tflite file
+typealias FileInfo = (name: String, extension: String)
+enum ModelFile {
+    
+    static let modelInfo: FileInfo = (name: "model", extension: "tflite")
+    // declare a interpreter
+    private var interpreter: Interpreter
+    
+    //initialize the variable
+    init?(modelFileInfo: FileInfo, threadCount: Int = 1){
+        let modelFilename = modelFileInfo.name
+        
+        guard let modelPath = Bundle.main.path(
+            forResource: modelFilename,
+            ofType: modelFileInfo.extension
+        )
+        else{
+            print("Failed to load the model file")
+            return nil
+        }
+        
+        // load the model from the file path
+        do {
+            interpreter = try interpreter(modelPath: modelPath)
+        }
+        catch let error
+        {
+            print("Failed to create the interpreter")
+            return nil
+        }
+        
+        
+       // pass the input and run the model
+        func runModel(withInput input: Float) -> Float? {
+            do {
+                // allocate tensors on the interpreter
+                try interpreter.allocateTensors()
+                // swift doesn't tensor data type, so we'll need to write the data directly
+                //memory in an unsafeMutableBufferPointer
+                var data: Float = input
+                let buffer: UnsafeMutableRawBufferPointer<Float> = UnsafeMutableRawBufferPointer(start: &data, count: 1)
+                
+                try interpreter.copy(Data(buffer: buffer), toInput(at: 0))
+                
+                try interpreter.invoke()
+                
+                let outputTensor = try interpreter.output(at: 0)
+                
+                let results: [Float32] = [Float32](unsafeData: outputTensor.data) ?? []
+                
+                guard let result = results.first else {
+                    return nil
+                }
+                return result
+            }
+            catch {
+                print(error)
+                return nil
+            }
+            
+        }
+    }
+}
+
+// add the array extension that handles the unsafe data and loads it into an array
+extension Array {
+  init?(unsafeData: Data) {
+    guard unsafeData.count % MemoryLayout<Element>.stride == 0
+        else { return nil }
+    #if swift(>=5.0)
+    self = unsafeData.withUnsafeBytes {
+      .init($0.bindMemory(to: Element.self))
+    }
+    #else
+    self = unsafeData.withUnsafeBytes {
+      .init(UnsafeBufferPointer<Element>(
+        start: $0,
+        count: unsafeData.count / MemoryLayout<Element>.stride
+      ))
+    }
+    #endif  // swift(>=5.0)
+  }
+}
+
+
--- tfliteApp/ViewController.swift
+++ tfliteApp/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textUserData: UITextField!
+    
+    @IBOutlet weak var textResult: UILabel!
+    
+    @IBAction func btnGo(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
